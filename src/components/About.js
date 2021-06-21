6cd2e8a504ac28c71eaee7969bb15ec58b103abb
Arinjay Gangwar <arinjay.gangwar100@gmail.com>
2021-06-21 20:15:02 +0530

    First Commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+           <h4>Version 1.0.0</h4>
+           <h4>Made with Passion By Arinjay Gangwar</h4>
+           <Link to='/'>Go Back</Link> 
+        </div>
+    )
+}
+
+export default About
